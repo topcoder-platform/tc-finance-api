53b095d794331d3137499447e886bb79c50819b8
Justin Gasper <jmgasper@gmail.com>
2025-09-25 16:03:28 +1000

    Better tweak to CORS for v6 testing

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,7 @@ async function bootstrap() {
     'http://localhost:3000',
     /\.localhost:3000$/,
     ENV_CONFIG.TOPCODER_WALLET_URL,
-    //Temporary for v6 testing
-    'https://*.topcoder-dev.com',
+    /^https:\/\/[\w-]+\.topcoder-dev\.com$/, // allow wallet-v6 and other subdomains
   ];
 
   const corsConfig: cors.CorsOptions = {
